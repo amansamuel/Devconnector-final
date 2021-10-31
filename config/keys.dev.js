ffdf8b4dd59199544370aea238588ee4191417f6
Aman <amansamicoder2021@gmail.com>
2021-10-31 04:44:18 -0700

    first commit

---
+++ config/keys.dev.js
@@ -0,0 +1,14 @@
+module.exports={
+    //  mongoURI:'mongodb://localhost/devconnector',
+    // mongoURI:'//devconnector:aman!samuel@cluster0.g5aax.mongodb.net/devconnector?retryWrites=true&w=majority',
+    SecretOrKey:'amanweb',
+
+
+    // amansamicoder
+
+    // username:'amansamicoderdev',
+    // password:'uTzuGnko4xoSFidE',
+
+
+    mongoURI:'mongodb+srv://amansamicoderdev:uTzuGnko4xoSFidE@cluster0.kxoz2.mongodb.net/Devconnector?retryWrites=true&w=majority'
+}
